4c62765aaf2986197e18296f28b2f6009175038f
Idand <idandamri@gmail.com>
2018-03-16 06:40:18 +0200

    changed toast to snackbar

--- app/src/main/java/com/guesswho/idan/androidguesswho/Activities/CharecterSelectorScreen.java
+++ app/src/main/java/com/guesswho/idan/androidguesswho/Activities/CharecterSelectorScreen.java
@@ -3,7 +3,6 @@ package com.guesswho.idan.androidguesswho.Activities;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
@@ -15,7 +14,6 @@ import com.guesswho.idan.androidguesswho.CharacterSelectObject;
 import com.guesswho.idan.androidguesswho.R;
 import com.guesswho.idan.androidguesswho.Utils;
 
-import java.lang.ref.WeakReference;
 import java.util.ArrayList;
 
 public class CharecterSelectorScreen extends BaseActivity {
@@ -38,23 +36,37 @@ public class CharecterSelectorScreen extends BaseActivity {
     private void initRv() {
 
         DataSet = new ArrayList<>();
-        if (Utils.getDataSet() == null || Utils.getDataSet().size() == 0) {
+        if(Utils.getDataSet() == null || Utils.getDataSet().size() == 0){
             Utils.initDataSet();
         }
         DataSet = Utils.getDataSet();
         charListRV = findViewById(R.id.char_selector_recycler_view);
         selectBtn = findViewById(R.id.select_card_tv);
         selectBtn.setText(R.string.select);
-        selectBtn.setOnClickListener(new onSelectClick(selectedCard, this));
+        selectBtn.setOnClickListener(selectClickListener);
         charListRV.setHasFixedSize(true);
-        charRVLayoutManager = new LinearLayoutManager(Utils.getContext(), LinearLayoutManager.HORIZONTAL, false);
+        charRVLayoutManager = new LinearLayoutManager(Utils.getContext(),LinearLayoutManager.HORIZONTAL,false);
         charListRV.setLayoutManager(charRVLayoutManager);
         charRVAdapter = new CharSelectorAdapter(DataSet, this);
         charListRV.setAdapter(charRVAdapter);
     }
 
+    public View.OnClickListener selectClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            if (selectedCard != null) {
+                Intent gameActivity = new Intent(getApplicationContext(), EasyGameActivity.class);
+                gameActivity.putExtra("selectedCard",selectedCard);
+                startActivity(gameActivity);
+                finish();
+            } else {
+                Snackbar.make(v,R.string.didnt_select_card, Snackbar.LENGTH_SHORT).show();
+//                Toast.makeText(CharecterSelectorScreen.this, R.string.didnt_select_card, Toast.LENGTH_SHORT).show();
+            }
+        }
+    };
 
-    public static class selectClickListener implements View.OnClickListener {
+    public static class selectClickListener implements View.OnClickListener{
 
         CharacterSelectObject selected = null;
 
@@ -76,34 +88,5 @@ public class CharecterSelectorScreen extends BaseActivity {
         this.selectedCard = selectedCard;
     }
 
-    public static class onSelectClick implements View.OnClickListener {
-
-        CharacterSelectObject selectedCard;
-        WeakReference<AppCompatActivity> activityRef;
-
-        public onSelectClick(CharacterSelectObject selectedCard, AppCompatActivity activity) {
-            this.selectedCard = selectedCard;
-            activityRef = new WeakReference<>(activity);
-        }
-
-        @Override
-        public void onClick(View view) {
-            AppCompatActivity context = null;
-            if (activityRef.get() != null) {
-                context = activityRef.get();
-            }
-            if(context != null) {
-                if (selectedCard != null) {
-                    Intent gameActivity = new Intent(context, EasyGameActivity.class);
-                    gameActivity.putExtra("selectedCard", selectedCard);
-                    context.startActivity(gameActivity);
-                    context.finish();
-                } else {
-                    Snackbar.make(view, R.string.didnt_select_card, Snackbar.LENGTH_LONG).show();
-//                    Toast.makeText(CharecterSelectorScreen.this, R.string.didnt_select_card, Toast.LENGTH_SHORT).show();
-                }
-            }
-        }
-    }
 
 }
